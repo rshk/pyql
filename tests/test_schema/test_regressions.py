f1cd411a2feb72efc2850340f87ad13eda7d9b45
Samuele Santi <sam@reinventsoftware.io>
2019-04-15 15:34:50 +0100

    Add test case to pinpoint #7

--- tests/test_schema/test_regressions.py
+++ tests/test_schema/test_regressions.py
@@ -27,3 +27,55 @@ def test_arguments_are_converted_to_snake_case():
 
     assert result.errors is None
     assert result.data == {'hello': 'IT WORKS'}
+
+
+def test_optional_argument_can_be_omitted():
+    # This works fine, even with issue #7
+
+    Query = Object('Query')
+
+    @Query.field('hello')
+    def resolve_hello(root, info, some_arg: str = 'DEFAULT') -> str:
+        return 'Hello {}'.format(some_arg)
+
+    schema = Schema(query=Query)
+    compiled = schema.compile()
+
+    # ----------------------------------------------------------------
+
+    result = graphql(
+        compiled, """
+        query {
+            hello
+        }
+        """)
+
+    assert result.errors is None
+    assert result.data == {'hello': 'Hello DEFAULT'}
+
+
+def test_optional_argument_can_be_none():
+    # Test for issue #7 (https://github.com/rshk/pyql/issues/7)
+
+    Query = Object('Query')
+
+    @Query.field('hello')
+    def resolve_hello(root, info, some_arg: str = None) -> str:
+        if some_arg is None:
+            return 'DEFAULT VALUE'
+        return 'Hello {}'.format(some_arg)
+
+    schema = Schema(query=Query)
+    compiled = schema.compile()
+
+    # ----------------------------------------------------------------
+
+    result = graphql(
+        compiled, """
+        query {
+            hello
+        }
+        """)
+
+    assert result.errors is None
+    assert result.data == {'hello': 'DEFAULT VALUE'}
